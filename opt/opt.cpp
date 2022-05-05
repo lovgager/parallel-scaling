75297d589a21683beba021a60a41406abba5d607
Oleg Gavrilov <oleg-gavrilov5@yandex.ru>
2022-05-05 22:35:38 +0300

    Add files via upload

---
+++ opt/opt.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <omp.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int T = atoi(argv[1]);
+    int N = atoi(argv[2]);
+    omp_set_num_threads(T);
+    double** a = new double*[N];
+    double* b = new double[N];
+    double* c = new double[N];
+
+    double start = omp_get_wtime();
+    #pragma omp parallel shared(N, a, b, c)
+	{
+		//initialization
+		#pragma omp for
+		for (int i = 0; i < N; ++i) {
+			a[i] = new double[N];
+			for (int j = 0; j < N; ++j) {
+				a[i][j] = cos(i * j + i + j);
+			}
+			b[i] = sin(i);
+		}
+
+		//matrix-vector product
+		#pragma omp for
+		for (int i = 0; i < N; ++i) {
+			c[i] = 0.0;
+			for (int j = 0; j < N; ++j) {
+				c[i] += a[i][j] * b[j];
+			}
+		}
+	}
+    double time = omp_get_wtime() - start;
+    printf("N = %d. T = %d. Wall time(s): %f \n", N, T, time);
+	
+	for (int i = 0; i < N; ++i)
+		delete[] a[i];
+	delete[] a;
+	delete[] b;
+	delete[] c;
+}
---
+++ test/test.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Request request;
+    MPI_Status status;
+    unsigned int N = atoi(argv[1]); //длина массива
+    unsigned int n = atoi(argv[2]); //количество блоков для передачи (если N делится на n)
+
+    double start = MPI_Wtime(); 
+
+    if (rank == 0) {
+        int* a = new int[N];
+
+        //получение массива по блокам
+        for (int i = 0; i < n; ++i)
+            MPI_Recv(a + i*N/n, N/n, MPI_INT, 1, 111, MPI_COMM_WORLD, &status);
+        //получаем оставшуюся часть, если N не делится на n
+        MPI_Recv(a + n*N/n, N%n, MPI_INT, 1, 111, MPI_COMM_WORLD, &status);
+
+        delete[] a;
+    }
+    if (rank == 1) {
+        //инициализация массива
+        int* a = new int[N];
+        for (int i = 0; i < N; ++i)
+            a[i] = i;
+
+        //пересылка массива по блокам
+        for (int i = 0; i < n; ++i)
+            MPI_Send(a + i*N/n, N/n, MPI_INT, 0, 111, MPI_COMM_WORLD);
+        //пересылка оставшейся части, если N не делится на n
+        MPI_Send(a + n*N/n, N%n, MPI_INT, 0, 111, MPI_COMM_WORLD);
+
+        delete[] a;
+    }
+
+    double end = MPI_Wtime(); 
+    if (rank == 0)
+        printf("N = %u. Number of blocks: %u. Time: %f s\n", N, n, end - start);
+
+    MPI_Finalize();
+}
